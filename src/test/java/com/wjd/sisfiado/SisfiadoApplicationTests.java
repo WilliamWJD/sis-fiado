1c6da806e164a330fd9a1c8450680754cdb98be7
WilliamWJD <william.jose.wjd@gmail.com>
2024-03-04 11:44:13 -0300

    iniciando projeto

---
+++ src/test/java/com/wjd/sisfiado/SisfiadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjd.sisfiado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisfiadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
